975e5d101037f0bfbccc97cca24e71b097b3a111
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-12-16 12:05:23 +0100

    FilmDB 504

---
+++ 5-7_mvc_i_EFCore/2023/FilmDB/FilmDB/Controllers/FilmController.cs
@@ -0,0 +1,90 @@
+﻿using FilmDB.Models;
+using FilmDB.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmDB.Controllers
+{
+    public class FilmController : Controller
+    {
+        private FilmManager _manager;
+        public FilmController(FilmManager filmManager) 
+        {
+            _manager = filmManager;
+        }
+        public IActionResult Index()
+        {
+            var films = _manager.GetFilms();
+
+            return View(films);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(FilmModel filmModel)
+        {
+            try
+            {   
+                _manager.AddFilm(filmModel);
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+
+                return View(filmModel);
+            }
+            
+            
+        }
+
+        [HttpGet]
+        public IActionResult Remove(int id)
+        {
+            var film = _manager.GetFilm(id);
+
+            return View(film);
+        }
+
+        [HttpPost]
+        public IActionResult RemoveConfirm(int id)
+        {
+            var filmToDelete = _manager.GetFilm(id);
+            if (filmToDelete != null) 
+            {
+                _manager.RemoveFilm(id);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Remove", id);
+            }  
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var film = _manager.GetFilm(id);
+
+            return View(film);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(FilmModel film)
+        {
+            _manager.UpdateFilm(film);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id) 
+        { 
+            var film = _manager.GetFilm(id);
+            return View(film);
+        }
+
+    }
+}
---
+++ 5-7_mvc_i_EFCore/2023/FilmDB/FilmDB/Repository/FilmManager.cs
@@ -0,0 +1,80 @@
+﻿using FilmDB.Data;
+using FilmDB.Models;
+
+namespace FilmDB.Repository
+{
+    public class FilmManager
+    {
+        private ApplicationDbContext _applicationDbContext;
+        public FilmManager(ApplicationDbContext applicationDbContext) 
+        {
+            _applicationDbContext = applicationDbContext;    
+        }
+        public FilmManager AddFilm(FilmModel filmModel)
+        {
+            try
+            {
+                _applicationDbContext.Add(filmModel);
+                _applicationDbContext.SaveChanges();
+                
+            }
+            catch (Exception)
+            {
+
+                filmModel.ID = 0;
+                //applicationDbContext.Add(filmModel);
+                _applicationDbContext.SaveChanges();
+            }
+            return this;
+        }
+
+        public FilmManager RemoveFilm(int id)
+        {
+            var filmToDel = GetFilm(id);
+            if (filmToDel != null)
+            {
+                _applicationDbContext.Films.Remove(filmToDel);
+                _applicationDbContext.SaveChanges();
+            }
+            return this;
+        }
+
+        public FilmManager UpdateFilm(FilmModel filmModel)
+        {
+            _applicationDbContext.Films.Update(filmModel);
+            _applicationDbContext.SaveChanges();
+            return this;
+        }
+
+        public FilmManager ChangeTitle(int id, string newTitle)
+        {
+            var filmToUpdate = GetFilm(id);
+            if (filmToUpdate != null)
+            {
+                try
+                {
+                    filmToUpdate.Title = newTitle;
+                    _applicationDbContext.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    filmToUpdate.Title = "Brak Tytułu";
+                    _applicationDbContext.SaveChanges();
+                }
+
+            }
+            return this;
+        }
+
+        public FilmModel GetFilm(int id)
+        {
+            return _applicationDbContext.Films.Single(x =>  x.ID == id);
+        }
+
+        public List<FilmModel> GetFilms()
+        {
+            return _applicationDbContext.Films.ToList();
+        }
+    }
+}
